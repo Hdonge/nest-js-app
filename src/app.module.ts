a544b9cc1e734cce882bff2ec051d146f1fc6511
Hdonge <harshal.donge@gmail.com>
2023-01-10 21:01:40 +0530

    Added Auth & Logger middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { CartsController } from './carts/carts.controller';
 import { CartsModule } from './carts/carts.module';
+import { PostsController } from './posts/posts.controller';
 import { PostsModule } from './posts/posts.module';
 import { ProductModule } from './product/product.module';
+import { AuthMiddleware } from './shared/middlewares/auth';
+import { LoggerMiddleware } from './shared/middlewares/logger';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
@@ -24,4 +28,10 @@ import { SharedModule } from './shared/shared.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer){
+      consumer
+      .apply(LoggerMiddleware).forRoutes(CartsController, PostsController)
+      .apply(AuthMiddleware).forRoutes(AppController);
+  }
+}
