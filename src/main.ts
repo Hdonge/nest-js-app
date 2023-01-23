524f5e963ca994b8b3ee4782f7861548817bb221
Harshal Donge <harshal.donge@gmail.com>
2023-01-23 19:35:02 +0530

    Implemented cache service, Refactored cache interceptor to use cache service & added tasks scheduling(#4)
    * Implemented cache service, Refactored cache interceptor to use cache service
    
    * Added versioning
    
    * Added Tasks and notifications scheduling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from "cookie-parser";
-
+import { VersioningType } from "@nestjs/common";
 import { AppModule } from './app.module';
 import { Logger } from './shared/services/logger';
 
@@ -9,11 +9,12 @@ async function bootstrap() {
     bufferLogs: true // false: framework will use default logger, true: will use custom logger for appstart logs too.
   });
 
-  console.log(process.env);
   const logger = new Logger();
   logger.mainContext = "NestAppStart";
   app.useLogger(logger);
-  
+  app.enableVersioning({
+    type: VersioningType.URI
+  });
   app.enableCors();
   app.use(cookieParser());
   app.setGlobalPrefix('api');
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,16 +1,28 @@
 import { HttpModule } from "@nestjs/axios";
-import { Module } from "@nestjs/common";
+import { CacheModule, Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
 
+import { CacheService } from "./services/cache";
 import { HttpService } from "./services/http";
 import { HttpClientBase } from "./services/http/httpClientBase";
 import { Logger } from "./services/logger";
+import { NotificationService } from "./services/scheduler-crons/notifications";
+import { TasksService } from "./services/scheduler-crons/tasks";
 
 @Module({
-    imports: [HttpModule.register({
-        timeout: 5000,
-        timeoutErrorMessage: "Request timeout! Please try again!",
-    })],
-    providers: [HttpClientBase, HttpService, Logger],
-    exports: [HttpService, Logger]
+    imports: [
+        HttpModule.register({
+            timeout: 5000,
+            timeoutErrorMessage: "Request timeout! Please try again!",
+        }),
+        CacheModule.register({
+            ttl: 10000,
+            max: 5,
+            isGlobal: true
+        }),
+        ScheduleModule.forRoot()
+    ],
+    providers: [HttpClientBase, HttpService, Logger, CacheService, TasksService, NotificationService],
+    exports: [HttpService, Logger, CacheService]
 })
-export class SharedModule {}
+export class SharedModule { }
